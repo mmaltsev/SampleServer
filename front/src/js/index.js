ddddcd25b97a3077afc7e794f60d17ed6acedb01
Maxim <maxim.maltsev@recogizer.de>
2017-12-05 18:01:49 +0100

    added axios as a dependency

--- front/src/js/index.js
+++ front/src/js/index.js
@@ -1,8 +1,21 @@
-function httpGet(theUrl) {
-  var xmlHttp = new XMLHttpRequest()
-  xmlHttp.open('GET', theUrl, false) // false for synchronous request
-  xmlHttp.send(null)
-  return xmlHttp.responseText
+function getRequest(url) {
+  axios.get(url)
+    .then(function(response) {
+      console.log('Requested data: ', response.data)
+    })
+    .catch(function(error) {
+      console.log('Error: ', error);
+    });
+}
+
+function postRequest(url, params) {
+  axios.post(url, params)
+    .then(function(response) {
+      console.log('Requested data: ', response.data)
+    })
+    .catch(function(error) {
+      console.log('Error: ', error);
+    });
 }
 
 function legendClick() {
